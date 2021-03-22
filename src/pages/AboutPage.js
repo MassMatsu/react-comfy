fd177d620a3ecc18d8eb33274108c2f8816be6f8
MassMatsu <masa10matsukawa@gmail.com>
2021-03-22 10:26:22 +0900

    implemented ErrorPage and AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Provident corrupti at assumenda laborum, facilis aperiam animi
+                vitae aliquam? Dolor similique cum explicabo fugit doloremque
+                enim nihil dicta deserunt, iure aliquid.
+              </p>
+            
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +53,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
